3dffc8ec4c14ef1d784017a8ed36731ed16269bd
naji <hafidanaji13@gmail.com>
2024-11-20 13:08:20 +0100

    secod commit micro services

--- customer-service/src/main/java/net/naji/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/net/naji/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package net.naji.customerservice;
 
+import net.naji.customerservice.config.CustomerConfigParams;
 import net.naji.customerservice.entities.Customer;
 import net.naji.customerservice.Repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
