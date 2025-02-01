1da326922c44c88d746917ee128c3fefbf547c59
naji <hafidanaji13@gmail.com>
2025-02-01 21:49:46 +0100

    Angular Products

--- angular_frontEnd_client/src/app/inventory/inventory.component.ts
+++ angular_frontEnd_client/src/app/inventory/inventory.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-inventory',
   standalone: false,
-  
+
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.css'
 })
-export class InventoryComponent {
-
+export class InventoryComponent implements OnInit{
+  products: any[] = [];
+  constructor(private http:HttpClient) {
+  }
+   ngOnInit() {
+    this.http.get("http://localhost:8888/inventory-service/api/products")
+      .subscribe({
+        next: (data: any) => {
+          console.log(data);  // Affiche les données pour vérification
+          if (data?._embedded?.products) {
+            this.products = data._embedded.products;  // Accède à la liste des clients
+          } else {
+            console.error('Aucun client trouvé ou format inattendu');
+          }
+        },
+        error:err => {
+          console.log(err);
+        }
+      })
+   }
 }
