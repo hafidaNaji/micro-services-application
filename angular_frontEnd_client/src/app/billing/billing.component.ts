e8e4a7e4528306308ddc48cae3a42a2987a54e3b
naji <hafidanaji13@gmail.com>
2025-02-03 15:33:52 +0100

    bills

--- angular_frontEnd_client/src/app/billing/billing.component.ts
+++ angular_frontEnd_client/src/app/billing/billing.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-billing',
   standalone: false,
-  
+
   templateUrl: './billing.component.html',
   styleUrl: './billing.component.css'
 })
-export class BillingComponent {
+export class BillingComponent implements OnInit {
+  bills: any[] = [];
+  facture: any; // Facture sélectionnée (déclaration explicite)
+  constructor(private http: HttpClient) {
+  }
 
+  ngOnInit() {
+    this.http.get("http://localhost:8888/billing-service/api/bills")
+      .subscribe({
+        next: (data: any) => {
+          console.log(data);  // Affiche les données pour vérification
+          if (data?._embedded?.bills) {
+            this.bills = data._embedded.bills;  // Accède à la liste des clients
+          } else {
+            console.error('Aucun client trouvé ou format inattendu');
+          }
+        },
+        error: err => {
+          console.log(err)
+        }
+      })
+  }
+
+  // Méthode pour afficher la facture par ID
+  display(b: any) {
+
+  }
 }
